b6fe3f1857e5afe7d41bfde47097fd16fb34136a
Bryvel <bryvel_1101@yahoo.com>
2022-03-08 22:55:27 -0500

    pruebas server

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 80
+var express=require('express');
+var app=express();
+var path=require('path');
+var morgan=require('morgan');
+var mongoose=require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//set app enviroment
+app.set('port',process.env.PORT || 80);
+//server u
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.use( express.static(__dirname + '/Public'));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+//Use middleware
+app.use(morgan('dev'));
+//DB connection
+mongoose.connect('mongodb+srv://sebas:sebas@cluster0.wh1fi.mongodb.net/veterinaria?retryWrites=true&w=majority')
+.then(db=>{console.log('db connected')})
+.catch(err=>{console.log(err)})
+//pruebas
+// routes
+var indexRoute=require('./routes/index');
+const { db } = require('./models/hueso');
+app.use('/',indexRoute);
+app.listen(app.get('port'),()=>{
+    console.log('server on port 80')
 })
