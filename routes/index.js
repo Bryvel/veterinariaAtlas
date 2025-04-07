e66106c81c8f0da7087bc8117d72f6ebe2ef6ba4
Bryan <bryvel_1101@yahoo.com>
2025-04-06 22:58:03 -0500

    test commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express=require('express');
 var router=express.Router();
 var hueso=require('../models/hueso');
+const fs = require('fs');
 
 router.get('/huesos/Generalidades',async (req,res)=>{
     var nombre=req.params.nombre;
@@ -45,6 +46,37 @@ router.get('/organos/container',async (req,res)=>{
 router.get('/3D',async (req,res)=>{
     
     res.render('3DContainer')
+})
+router.get('/pdfMiologia',async (req,res)=>{
+    
+    res.render('containerPdfAtlasMiologia')
+})
+router.get('/pdfOsteologia',async (req,res)=>{
+    
+    res.render('containerPdfAtlasOsteologia')
+})
+
+router.get('/AtlasOsteologia', (req, res) => {
+    const path = './Public/ico/Atlas osteologia.pdf'
+    if (fs.existsSync(path)) {
+        res.contentType("application/pdf");
+        fs.createReadStream(path).pipe(res)
+    } else {
+        res.status(500)
+        console.log('File not found')
+        res.send('File not found')
+    }
+})
+router.get('/AtlasMiologia', (req, res) => {
+    const path = './Public/ico/Atlas miologia.pdf'
+    if (fs.existsSync(path)) {
+        res.contentType("application/pdf");
+        fs.createReadStream(path).pipe(res)
+    } else {
+        res.status(500)
+        console.log('File not found')
+        res.send('File not found')
+    }
 })
 
 module.exports=router;
